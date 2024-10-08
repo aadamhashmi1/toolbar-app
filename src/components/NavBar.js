80fa3c1ffe35ef0f5b176e00374d682d0c1a2649
aadamhashmi1 <muhammadaadamhashmi@gmail.com>
2024-10-08 08:47:26 +0500

    navbar final

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,6 @@ const NavBar = () => {
             <div className="container mx-auto flex justify-between items-center">
                 <Link to="/" className="text-white text-lg font-bold">Quote App</Link>
                 <Link to="/project">
-                    <button className="bg-white text-blue-500 px-4 py-2 rounded">Go to Project</button>
                 </Link>
             </div>
         </nav>
